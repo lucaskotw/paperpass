019c10a094aec7795fb07475b4effb57bb415d2c
Lucas Ko <lucaskointw@gmail.com>
2013-10-03 00:05:16 +0800

    create LICENSE, README.rst and setup.py

---
+++ README.rst
@@ -0,0 +1,9 @@
+==========
+Paper Pass
+==========
+
+Paper Pass is based on the idea of S. Keshav paper, How to Read a Paper. It provides a way to dump a json file of five Cs analysis while user is doing the first pass.
+
+You may install Paper Pass as follow::
+
+  pip install paperpass
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='PaperPass',
+    version='0.1',
+    author='Lucas Ko',
+    author_email='lucaskointw@gmail.com',
+#    packages=['paperpass'],
+    license='LICENSE',
+    py_modules=['paperpass'],
+    description='Paper Pass First Pass Tools',
+    long_description=open('README.rst').read(),
+)
