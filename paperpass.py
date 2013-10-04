81bc9eb5fd5991a1cf41e61131a8af018afd2ffe
Lucas Ko <lucaskointw@gmail.com>
2013-10-05 00:38:29 +0800

    create 5 set method

--- paperpass.py
+++ paperpass.py
@@ -3,21 +3,34 @@ import json
 __version__ = "0.1"
 
 class PaperPass:
-    """ This Class constructs first pass of a paper """
-    # class var
-    outline = {}
-    category = {}
+    " This Class constructs first pass of a paper "
 
     def __init__(self, outline):
         self.outline = outline
+
+    def setCategory(self, category):
+        self.category = category
+
+    def setContext(self, context):
+        self.context = context
+
+    def setCorrectness(self, correctness):
+        self.correctness = correctness
+
+    def setContributions(self, contributions):
+        self.contributions = contributions
+
+    def setClarity(self, clarity):
+        self.clarity = clarity
 
     def outputjson(self, filename):
         fp = open(filename, 'w')
         json.dump(self.outline, fp)
 
-
+""""
 if __name__ == "__main__":
     import sys
 
     a = PaperPass({3:2,2:1})
     a.outputjson(sys.argv[1])
+"""
